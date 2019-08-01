d5133b2747f197a8ff1771688d891963dbfea1f1
Schevene <Schevene.Singh@MyGeorgian.ca>
2019-07-31 20:20:18 -0400

    added basic necessities for adding users such as middelwares and authentication, need editing

---
+++ middlewares/requireAuth.js
@@ -0,0 +1,10 @@
+
+const requireAuth = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      next();
+    } else {
+      res.redirect('/login');
+    }
+  };
+  
+  module.exports = requireAuth;
