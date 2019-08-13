2d9e7f7f65085cda8247bf3d7d0b3414c83f8261
AlyssaB283 <Alyssa.Brown2@MyGeorgian.ca>
2019-08-12 23:45:46 -0400

    touches

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/styles/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); // <- This will use the contents of 'bootstrap/dist/css' which is placed in your node_modules folder as if it is in your '/styles/css' directory.
 
 //Authentication Startup/Setup
 app.use(
